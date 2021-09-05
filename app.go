61d5b0a6ccf0ba040c048689e0af784e79263cd5
Kornelius Septa Jasa Sipayung <writecodex09@gmail.com>
2021-09-05 17:16:41 +0700

    app pertama
    app pertama

---
+++ app.go
@@ -0,0 +1,10 @@
+package main 
+
+import (
+	"fmt"
+	salam "github.com/writecodex09/salam"
+)
+
+func main() {
+	fmt.Println(salam.SalamSapa())
+}
---
+++ go.mod
@@ -0,0 +1,5 @@
+module github.com/writecodex09/app
+
+go 1.14
+
+require github.com/writecodex09/salam v1.0.0 // indirect
